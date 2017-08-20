b56c19fa195e43cf95f948db011156b63bf9a7a1
11 <Administrator@XTM-01705032220>
2017-08-20 20:21:49 +0800

    11

--- src/main/java/cn/gson/oasys/OasysApplication.java
+++ src/main/java/cn/gson/oasys/OasysApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OasysApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(OasysApplication.class, args);
 	}
 }
