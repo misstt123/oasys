807d957fce23344c630cde6d4c74b411698dc9cb
luoxiang <luoxiang@192.168.2.21>
2017-09-29 13:09:29 +0800

    呀呀呀

--- src/main/java/cn/gson/oasys/OasysApplication.java
+++ src/main/java/cn/gson/oasys/OasysApplication.java
@@ -1,9 +1,5 @@
 package cn.gson.oasys;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-import org.junit.Test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -13,10 +9,5 @@ public class OasysApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OasysApplication.class, args);
-	}
-	
-	@Test
-	public void dfsa(){
-		
 	}
 }
