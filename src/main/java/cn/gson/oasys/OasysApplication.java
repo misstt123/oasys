ff30877fd0207e336d26b339a1ba322e52f319cf
luoxiang <luoxiang@110.52.11.56>
2017-09-30 12:23:00 +0800

    整合

--- src/main/java/cn/gson/oasys/OasysApplication.java
+++ src/main/java/cn/gson/oasys/OasysApplication.java
@@ -9,14 +9,5 @@ public class OasysApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OasysApplication.class, args);
-<<<<<<< HEAD
-	}
-	
-	@Test
-	public void dfsa(){
-		String s1="18:49:19";String s2="18:29:39";
-		System.out.println(s1.compareTo(s2));
-=======
->>>>>>> branch 'yqs' of https://git.oschina.net/aaluoxiang/oa_system.git
 	}
 }
